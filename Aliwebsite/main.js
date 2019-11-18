fe36ce33469bb877dc185b25f01418465ee1a94d
Arel <32299070+alitunaarel@users.noreply.github.com>
2019-11-18 16:10:12 +0100

    Add files via upload

--- Aliwebsite/main.js
+++ Aliwebsite/main.js
@@ -1,1 +1,34 @@
-console.log("sqdq")
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
+
+
+$(document).ready(function(){
+
+    $("#btnup").click(function(){
+        $('html, body').animate({scrollTop: 0 },80);
+    });
+    $('#menuLink1').click(function(){
+        $('html, body').animate({scrollTop: $('#competence').offset().top},120);
+    });
+
+
+    
+    
+    $(window).scroll(function() {
+     
+      if($(this).scrollTop() > 20) { 
+          $('nav').addClass('solid');
+      } else {
+          $('nav').removeClass('solid');
+      }
+    });
+});
